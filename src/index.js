2714dbfecdcd20b121c8a0813023d891a0b023c5
Leandro <leandro.nishijima@gmail.com>
2017-04-26 15:47:36 -0300

    implemented video_details

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details'
 
 const API_KEY = 'AIzaSyBfEWb3HWUrMqUXaFH3gjfwdQzTqLsg0Hg';
 
@@ -21,7 +22,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos}/>
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);	
 	}
